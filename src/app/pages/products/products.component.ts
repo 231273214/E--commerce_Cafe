65132c0c03a0bcfd56a5698be21b7d4f64bbaf1c
BautistaMabel <mdbautista@uniboyaca.edu.co>
2025-05-12 13:27:12 -0500

    Ajustes src y productos

--- src/app/pages/products/products.component.ts
+++ src/app/pages/products/products.component.ts
@@ -1,11 +1,58 @@
 import { Component } from '@angular/core';
+import { ProductService, Product } from '../../services/product.service';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-products',
-  imports: [],
   templateUrl: './products.component.html',
-  styleUrl: './products.component.scss'
+  styleUrls: ['./products.component.scss'],
+  standalone: true,
+  imports: [CommonModule, FormsModule]
 })
 export class ProductsComponent {
+  products: Product[] = [];
+  filteredProducts: Product[] = [];
+  searchQuery: string = '';
+  sortOption: string = ''; 
 
+  constructor(private productService: ProductService) {}
+
+  ngOnInit() {
+    this.products = this.productService.getProducts();
+    this.filteredProducts = [...this.products];
+  }
+
+  search() {
+    const query = this.searchQuery.toLowerCase();
+    this.filteredProducts = this.products.filter(product =>
+      product.name.toLowerCase().includes(query)
+    );
+    this.applySort();
+  }
+
+  applySort() {
+    if (this.sortOption === 'price') {
+      this.filteredProducts.sort((a, b) => a.price - b.price);
+    } else if (this.sortOption === 'name') {
+      this.filteredProducts.sort((a, b) => a.name.localeCompare(b.name));
+    }
+  }
+
+  filteredByCategory(category: string): Product[] {
+    const query = this.searchQuery.toLowerCase();
+    let products = this.products.filter(p =>
+      p.category === category &&
+      p.name.toLowerCase().includes(query)
+    );
+
+    if (this.sortOption === 'price') {
+      products.sort((a, b) => a.price - b.price);
+    } else if (this.sortOption === 'name') {
+      products.sort((a, b) => a.name.localeCompare(b.name));
+    }
+
+    return products;
+  }
 }
+
